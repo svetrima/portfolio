dc76c3d10cd7fbb72c67bb227236e4ac8b8fef6b
Svetrima <svetsmetanina@gmail.com>
2022-05-30 22:24:57 +0300

    feat:add script for translate page

--- photographer/js/script.js
+++ photographer/js/script.js
@@ -1,3 +1,4 @@
+import i18Obj from './translate.js';
 const links = document.querySelector('.navigation_bar');
 const openCloseBurger = document.getElementById('burger');
 const burgerMenu = document.querySelector('.burger_menu');
@@ -6,27 +7,58 @@ const body = document.querySelector('body');
 const html = document.querySelector('html');
 
 links.addEventListener('click', (event) => {
-    links.querySelectorAll('a').forEach(el => el.classList.remove('active'));
-    event.target.classList.add('active');
-  });
+  links.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+  event.target.classList.add('active');
+});
 
-  openCloseBurger.addEventListener('click', (e) => {
-    e.preventDefault();
-    burgerMenu.classList.toggle('show');
-    openCloseMenu.classList.toggle('navigation_bar__show');
-   /* body.classList.toggle('overlay');*/
-    body.style.overflowY = 'hidden';
-    html.style.overflowY = 'hidden';
-    if (!e.target.closest('.show')) {
-      body.style.overflowY = 'visible';
-    }
-  });
+openCloseBurger.addEventListener('click', (e) => {
+  e.preventDefault();
+  burgerMenu.classList.toggle('show');
+  openCloseMenu.classList.toggle('navigation_bar__show');
+  /* body.classList.toggle('overlay');*/
+  body.style.overflowY = 'hidden';
+  html.style.overflowY = 'hidden';
+  if (!e.target.closest('.show')) {
+    body.style.overflowY = 'visible';
+  }
+});
 
-  window.addEventListener('click', (e) => {
-    if (!e.target.closest('.burger_menu')) {
-      burgerMenu.classList.remove('show');
-      openCloseMenu.classList.remove('navigation_bar__show');
-      body.style.overflowY = 'visible';
-      html.style.overflowY = 'visible';
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.burger_menu')) {
+    burgerMenu.classList.remove('show');
+    openCloseMenu.classList.remove('navigation_bar__show');
+    body.style.overflowY = 'visible';
+    html.style.overflowY = 'visible';
+  }
+});
+
+/*translate*/
+const languages = document.querySelector('.language');
+const btnsRuEn = document.querySelectorAll('.btn_lang');
+const dataEl = document.querySelectorAll('[data-i18]');
+console.log(btnsRuEn)
+
+
+
+languages.addEventListener('click', (e) => {
+  e.target.nodeName !== 'BUTTON' ? '' :
+    btnsRuEn.forEach((btn) => btn.classList.remove('active'));
+  e.target.classList.add('active');
+  getTranslate(e.target.dataset.lang);
+  localStorage.setItem('language', e.target.dataset.lang);
+});
+
+const getTranslate = (language) => {
+  dataEl.forEach((el) => {
+    el.textContent = i18Obj[language][el.dataset.i18];
+    if (el.placeholder) {
+      el.placeholder = i18Obj[language][el.dataset.i18];
+      el.textContent = '';
     }
-  });
+    if (el.value) {
+      el.value = i18Obj[language][el.dataset.i18];
+      el.textContent = '';
+    }
+  })
+};
+getTranslate(localStorage.getItem('language'));
