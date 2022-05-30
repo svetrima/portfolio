c30cb3407d180e473d3cead6af38551b6c669f63
Svetrima <svetsmetanina@gmail.com>
2022-05-30 19:26:48 +0300

    fix: add option for placeholder

--- photographer/js/translate.js
+++ photographer/js/translate.js
@@ -12,7 +12,7 @@ const i18Obj = {
       'skill-text-1': 'High-quality photos in the studio and on the nature',
       'skill-title-2': 'Video shooting',
       'skill-text-2': 'Capture your moments so that they always stay with you',
-      'skill-title-3': 'Rotouch',
+      'skill-title-3': 'Retouch',
       'skill-text-3': 'I strive to make photography surpass reality',
       'skill-title-4': 'Audio',
       'skill-text-4': 'Professional sounds recording for video, advertising, portfolio',
@@ -37,7 +37,10 @@ const i18Obj = {
       'price-description-3-span-5': 'Make up, visage, hairstyle',
       'order': 'Order shooting',
       'contact-me': 'Contact me',
-      'send-message': 'Send message'
+      'send-message': 'Send message',
+      'email': 'E-mail',
+      'phone': 'Phone',
+      'message': 'Message'
     },
     'ru': {
       'skills': 'Навыки',
@@ -77,7 +80,10 @@ const i18Obj = {
       'price-description-3-span-5': 'Макияж, визаж, прическа',
       'order': 'Заказать съемку',
       'contact-me': 'Свяжитесь со мной',
-      'send-message': 'Отправить'
+      'send-message': 'Отправить',
+      'email': 'Эл.почта',
+      'phone': 'Телефон',
+      'message': 'Сообщение'
     }
   }
   export default i18Obj;
